32063b7c4ee4e1b8a73b5be2f4c8510ba3fcbcd7
gouuki <bgr.drms04@gmail.com>
2026-05-08 23:06:20 +0200

    Integrated logger into DateOrdner and refined path handling with timestamp

--- DateiOrdnen.cpp
+++ DateiOrdnen.cpp
@@ -4,6 +4,9 @@
 #include <format>
 #include <string>
 #include "DateiOrdnen.h"
+#include <ctime>
+#include <chrono>
+#include <fstream>
 
 namespace fs = std::filesystem;
 
@@ -39,15 +42,20 @@ void FileManager::organize(const fs::path& path)
         fs::path target_path = mk_folder / entry.path().filename();
         
         target_path = getSafePath(target_path);
-        
+
         try
         {
             fs::rename(entry.path() , target_path );
+            mylogger.writeLog(entry.path().filename().string() , target_path.string()
+                                ,path.string());
         }
         catch(const fs::filesystem_error & e)
         {
+            mylogger.writeLog(entry.path().filename().string(), "Error :" + std::string(e.what()),
+                            path.string());
             std::cerr << e.what() << '\n';
         }
+
         
     
 
@@ -76,4 +84,26 @@ fs::path FileManager::getSafePath(fs::path target)
     }
 
     return new_path;
+}
+
+void Logger::writeLog(const std::string& data_name , const std::string& target_folder,
+                const std::string& log_folder)
+{
+    auto time = std::chrono::system_clock::now();
+    auto time_now = std::chrono::floor<std::chrono::seconds>(time);
+    std::string date = std::format({":%d-%m-%Y %H:%M:%S"}, time_now);
+
+    fs::path log_path = fs::path(log_folder) / "history.log";
+
+    std::ofstream log(log_path, std::ios::app);
+    if(!log.is_open())
+    {
+        std::cerr<< "Error: cannot log file open!";
+        return;
+    }
+
+    log<< time_now << " " << data_name << " to "<< target_folder<< "\n";
+    log.close();
+
+
 }
--- DateiOrdnen.h
+++ DateiOrdnen.h
@@ -1,14 +1,25 @@
 #pragma once
 #include <filesystem>
+#include <string>
 
 namespace fs = std::filesystem;
 
-namespace FileManager {
+class Logger{
+    public:
+    void writeLog(const std::string& data_name , const std::string& target_folder , 
+                    const std::string& log_folder);
 
-    void organize(const fs::path& path);
+};
+
+class FileManager {
+    private:
     fs::path getSafePath(fs::path target);
+    Logger mylogger;
 
-}
+    public:  
+    void organize(const fs::path& path);
+   
+};
 
 
 
